2961d0f76e00608df286863e849a071c56b99f09
flowv <vincentnowak@me.com>
2018-04-08 22:17:46 +0100

    Position Dots added

--- sketch.js
+++ sketch.js
@@ -1,37 +1,46 @@
-const mapboxkey = "pk.eyJ1IjoidmluY2VudG4iLCJhIjoiY2pmbno1dTE4MXpkOTJ5bzBiczhrdG9zcyJ9.u63fT4mJOxGUy2GQ_ZNUBg"
-const accesskey= "7EIBQINL4B5MBZ7P7PT2G2OFEZTKNEUBTJEUBRMSPPRJN3TLSQHA"
-const secretkey= "1AST67SOSHLVY647U6LSCJFG8VCIS9TOVCV5VBW16F16C0Y2MFBJJA0YOLQO3XSI"
+var mapboxkey = "pk.eyJ1IjoidmluY2VudG4iLCJhIjoiY2pmbno1dTE4MXpkOTJ5bzBiczhrdG9zcyJ9.u63fT4mJOxGUy2GQ_ZNUBg";
+var table;
+var points = [];
 
 
-const options = {
-  lat: 51.535323,
-  lng: -0.040133,
-  zoom: 15,
-  studio: true,
-  style: 'mapbox://styles/vincentn/cjfpl6mlq0iht2sqj5bx4x1x9'
-  // pitch: 30,
-};
+var options = {lat:
+51.535323, lng: 
+-0.040133, zoom:
+15, studio: 
+true, style:
+'mapbox://styles/vincentn/cjfpl6mlq0iht2sqj5bx4x1x9'};
 
-const mappa = new Mappa('Mapbox', mapboxkey)
-let myMap;
-let canvas;
-let table;
+var mappa = new Mappa('Mapbox', mapboxkey);
+var myMap;
+var canvas;
 
-
-
-function preload() {
-table = loadTable("openpaths_vincent76.csv","csv","header");
+function preload () {
+  table = loadTable('data/openpaths_vincent76.csv', 'csv', 'header');
 }
 
-function setup(){
-  canvas = createCanvas(600,600);
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
   myMap = mappa.tileMap(options);
   myMap.overlay(canvas);
-  // print(table.getRowCount() + ' total rows in table');
-  // print(table.getColumnCount() + ' total columns in table');
-  // print(table.getColumn('name'));
+  strokeWeight(1);
+  stroke(17, 38, 245);
+  noFill();
+  myMap.onChange(drawDots);
 }
 
-function draw(){
+function draw() {
+}
 
+
+function drawDots() {
+  clear();
+  for ( var i = 0; i < table.getRowCount(); i++) {
+    var row = table.getRow(i);
+    var latitude = Number(table.getString(i, 'lat'));
+    var longitude = Number(table.getString(i, 'lon'));
+    if (myMap.map.getBounds().contains([latitude, longitude])) {
+      var pos = myMap.latLngToPixel(latitude, longitude);
+      ellipse(pos.x, pos.y, 5, 5);
+    }
+  }
 }
